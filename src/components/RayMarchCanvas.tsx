a9fe94055dde99a8fc2c0e8b5d78f0fcd7694f6b
Arun Joseph <arunjoseph3007@gmail.com>
2024-02-03 21:35:17 +0530

    Uniforms working

--- src/components/RayMarchCanvas.tsx
+++ src/components/RayMarchCanvas.tsx
@@ -1,14 +1,13 @@
 import * as webgl from "@/utils/webgl";
-import { ElementRef, useEffect, useRef } from "react";
+import { ElementRef, MouseEventHandler, useEffect, useRef } from "react";
 
 const vertexSrc = `#version 300 es
 in vec4 a_position;
 out vec2 pos;
-uniform vec2 u_resolution;
 
 void main() {
   gl_Position = a_position;
-  pos = (gl_Position.xy*0.5)*u_resolution;
+  pos = gl_Position.xy;
 }
 `;
 
@@ -16,23 +15,33 @@ const fragmentSrc = `#version 300 es
 precision highp float;
 in vec2 pos;
 out vec4 outColor;
+uniform vec3 u_mouse;
+uniform vec2 u_resolution;
 
 void main(){
     outColor = vec4(pos.x,pos.y,0,1);
+    outColor.xy -= (u_mouse.xy+0.5)/u_resolution;
 }
 `;
 const positions = [-1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1];
+
+let canvasTopLeftLoc = { x: 0, y: 0 };
+let gl: WebGL2RenderingContext | null;
+let isMouseDown: boolean = false;
+let mousePosition = { x: 0, y: 0 };
+let mousePositionUniformLocation: WebGLUniformLocation | null = null;
 
 export default function RayMarchCanvas() {
   const canvasRef = useRef<ElementRef<"canvas">>(null);
 
   useEffect(() => {
     if (!canvasRef.current) return;
+    const rect = canvasRef.current.getBoundingClientRect();
+    canvasTopLeftLoc = { x: rect.left, y: rect.bottom };
 
-    const gl = canvasRef.current.getContext("webgl2");
+    gl = canvasRef.current.getContext("webgl2");
     if (!gl) return;
 
-    const { width, height } = canvasRef.current.getBoundingClientRect();
     const vertexShader = webgl.createShader(gl, gl.VERTEX_SHADER, vertexSrc);
     const fragmentShader = webgl.createShader(
       gl,
@@ -56,6 +65,7 @@ export default function RayMarchCanvas() {
       program,
       "u_resolution"
     );
+    mousePositionUniformLocation = gl.getUniformLocation(program, "u_mouse");
 
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -66,8 +76,8 @@ export default function RayMarchCanvas() {
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // gl.canvas.width = width;
-    // gl.canvas.height = height;
+    gl.canvas.width = rect.width - 10;
+    gl.canvas.height = rect.height - 10;
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 0);
@@ -76,11 +86,54 @@ export default function RayMarchCanvas() {
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    const animId = requestAnimationFrame(renderLoop);
+
+    return () => {
+      cancelAnimationFrame(animId);
+    };
   }, [canvasRef.current]);
+
+  const handlMouseDown: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    isMouseDown = true;
+    mousePosition = {
+      x: e.clientX - canvasTopLeftLoc.x,
+      y: canvasTopLeftLoc.y - e.clientY,
+    };
+  };
+  const handlMouseMove: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (isMouseDown) {
+      mousePosition = {
+        x: e.clientX - canvasTopLeftLoc.x,
+        y: canvasTopLeftLoc.y - e.clientY,
+      };
+    }
+  };
+  const handlMouseUp: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    isMouseDown = false;
+  };
+
+  const renderLoop: FrameRequestCallback = (timeStamp) => {
+    if (gl) {
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+      gl.uniform3f(
+        mousePositionUniformLocation,
+        mousePosition.x,
+        mousePosition.y,
+        isMouseDown ? 1 : 0
+      );
+    }
+
+    window.requestAnimationFrame(renderLoop);
+  };
 
   return (
     <canvas
       ref={canvasRef}
+      onMouseDown={handlMouseDown}
+      onMouseMove={handlMouseMove}
+      onMouseUp={handlMouseUp}
       className="self-stretch flex-1 border border-zinc-800 rounded-md"
     />
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,24 @@ import RayMarchCanvas from "@/components/RayMarchCanvas";
 import ShapeEditor from "@/components/ShapeEditor";
 import ShapesTree from "@/components/ShapesTree";
 import { Montserrat } from "next/font/google";
+import Head from "next/head";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      style={montserrat.style}
-      className="flex min-h-screen items-center justify-between gap-2 p-2"
-    >
-      <ShapesTree />
-      <RayMarchCanvas />
-      <ShapeEditor />
-    </main>
+    <>
+      <Head>
+        <title>March Labs</title>
+      </Head>
+      <main
+        style={montserrat.style}
+        className="flex min-h-screen items-center justify-between gap-2 p-2 overflow-hidden"
+      >
+        <ShapesTree />
+        <RayMarchCanvas />
+        <ShapeEditor />
+      </main>
+    </>
   );
 }
