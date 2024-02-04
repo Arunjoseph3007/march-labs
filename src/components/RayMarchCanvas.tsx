2d83fa6cae1dea7eeb89a829ec2a1afcc791dc64
Arun Joseph <arunjoseph3007@gmail.com>
2024-02-04 17:44:22 +0530

    Basic ray marching working

--- src/components/RayMarchCanvas.tsx
+++ src/components/RayMarchCanvas.tsx
@@ -18,9 +18,69 @@ out vec4 outColor;
 uniform vec3 u_mouse;
 uniform vec2 u_resolution;
 
+mat2 rot(float a){
+  float c = cos(a);
+  float s = sin(a);
+  return mat2(c,s,-s,c);
+}
+
+#define MAX_STEPS 100
+#define MAX_DIST 100.
+#define MIN_DIST 0.01
+#define PI 3.141592
+
+vec3 GetRayDir(vec2 uv, vec3 from, vec3 lookat, float fov) {
+  vec3 upDir = vec3(0,1,0);
+  vec3 camDir = normalize(lookat-from);
+  vec3 r = normalize(cross(upDir, camDir));
+  vec3 u = cross(camDir,r);
+  vec3 c = camDir*fov;
+  vec3 i = c + uv.x*r + uv.y*u;
+  return normalize(i);
+}
+
+float circleSDF(vec3 pos, vec3 center, float r){
+  return length(center - pos) - r;
+}
+
+float getDist(vec3 pos){
+  return circleSDF(pos, vec3(0), 2.);
+}
+
+float rayMarch(vec3 org, vec3 dir){
+  float depth = 0.0;
+
+  for(int i = 0; i < MAX_STEPS; i++){
+    vec3 pos = org + depth * dir;
+    float t = getDist(pos);
+    depth += t;
+    if(t < MIN_DIST || depth > MAX_DIST) break;
+  }
+
+  return depth;
+}
+
 void main(){
-    outColor = vec4(pos.x,pos.y,0,1);
-    outColor.xy -= (u_mouse.xy+0.5)/u_resolution;
+  vec3 col = vec3(1);
+  vec3 lightSrc = normalize(vec3(1));
+  
+  vec2 fragCoord = pos*u_resolution;
+  vec2 uv = fragCoord/u_resolution.y;
+  vec2 m = u_mouse.xy/u_resolution.xy;
+
+  vec3 org = vec3(0, 0, -5);
+  org.xz *= rot(-m.x*2.0*PI);
+  vec3 dir = GetRayDir(uv, org, vec3(0), .8);
+  
+  float t = rayMarch(org, dir);
+  if(t<MAX_DIST){
+    vec3 pos = org + dir * t;
+    col = vec3(1,0,1);
+    col *= dot(lightSrc, -normalize(pos))/2.0+0.5;
+  }
+
+  outColor.xyz = col;
+  outColor.w = 1.0;
 }
 `;
 const positions = [-1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1];
