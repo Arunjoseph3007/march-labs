4e30e3e329064ab9d83624998c79946322158e44
Arun Joseph <arunjoseph3007@gmail.com>
2024-02-06 20:58:43 +0530

    Slightly better looking Camera editor

--- src/components/common/Vec3Editor.tsx
+++ src/components/common/Vec3Editor.tsx
@@ -1,6 +1,8 @@
+import { ChevronDownIcon } from "@/icons/chevronDown";
 import { MinusIcon } from "@/icons/minus";
 import { PlusIcon } from "@/icons/plus";
 import { IVec3 } from "@/types/vec";
+import { useState } from "react";
 
 type IVec3Editor = {
   label: string;
@@ -9,48 +11,68 @@ type IVec3Editor = {
 };
 
 export default function Vec3Editor({ label, onChange, value }: IVec3Editor) {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
-    <div>
-      <p>{label}</p>
-      <div>
-        {([0, 1, 2] as const).map((idx) => (
-          <div className="flex gap-2 items-center my-1" key={idx}>
-            <button
-              onClick={() => {
-                const newVal: IVec3 = [...value];
-                newVal[idx] -= 0.1;
-                onChange(newVal);
-              }}
-              className="bg-zinc-900 rounded-full hover:bg-zinc-800 transition-all"
-            >
-              <MinusIcon />
-            </button>
-            <input
-              type="range"
-              value={value[idx]}
-              onChange={(e) => {
-                const newVal: IVec3 = [...value];
-                newVal[idx] = +e.target.value;
-                onChange(newVal);
-              }}
-              min={-10}
-              max={10}
-              step={0.1}
-            />
-            <button
-              onClick={() => {
-                const newVal: IVec3 = [...value];
-                newVal[idx] += 0.1;
-                onChange(newVal);
-              }}
-              className="bg-zinc-900 rounded-full hover:bg-zinc-800 transition-all"
-            >
-              <PlusIcon />
-            </button>
-            <span className="text-right">{value[idx].toFixed(2)}</span>
-          </div>
-        ))}
+    <div className="my-3">
+      <div className="flex items-center gap-1">
+        <p>{label}</p>
+        <span className="flex-1 text-right text-xs text-zinc-400">
+          {collapsed ? value.map((e) => e.toFixed(2)).join(", ") : ""}
+        </span>
+        <button
+          onClick={() => setCollapsed((p) => !p)}
+          className={`text-zinc-500 scale-75 transition-all ${
+            !collapsed && "rotate-180"
+          }`}
+        >
+          <ChevronDownIcon />
+        </button>
       </div>
+      {!collapsed && (
+        <div className="mt-2 mb-4">
+          {([0, 1, 2] as const).map((idx) => (
+            <div className="flex gap-2 items-center my-1" key={idx}>
+              <button
+                onClick={() => {
+                  const newVal: IVec3 = [...value];
+                  newVal[idx] -= 0.1;
+                  onChange(newVal);
+                }}
+                className="text-zinc-700 rounded-full hover:text-zinc-500 transition-all"
+              >
+                <MinusIcon />
+              </button>
+              <input
+                type="range"
+                className="appearance-none cursor-pointer h-[3px] rounded-full bg-zinc-400 accent-zinc-200"
+                value={value[idx]}
+                onChange={(e) => {
+                  const newVal: IVec3 = [...value];
+                  newVal[idx] = +e.target.value;
+                  onChange(newVal);
+                }}
+                min={-10}
+                max={10}
+                step={0.1}
+              />
+              <button
+                onClick={() => {
+                  const newVal: IVec3 = [...value];
+                  newVal[idx] += 0.1;
+                  onChange(newVal);
+                }}
+                className="text-zinc-700 rounded-full hover:text-zinc-500 transition-all"
+              >
+                <PlusIcon />
+              </button>
+              <span className="flex-1 text-right text-xs text-zinc-400">
+                {value[idx].toFixed(2)}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/icons/chevronDown.tsx
+++ src/icons/chevronDown.tsx
@@ -1,4 +1,4 @@
-export const MinusIcon = () => (
+export const ChevronDownIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
@@ -7,7 +7,7 @@ export const MinusIcon = () => (
   >
     <path
       fillRule="evenodd"
-      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z"
+      d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
       clipRule="evenodd"
     />
   </svg>
